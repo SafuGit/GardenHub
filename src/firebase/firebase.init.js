a6dd4fc10280e4d71eb5925919717b1d4ec0226d
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-05-20 17:48:02 +0600

    hide firebase credentials

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDew1hZW6ilCyAQalhIkPuClt3XdxiueYg",
-  authDomain: "safu-a10.firebaseapp.com",
-  projectId: "safu-a10",
-  storageBucket: "safu-a10.firebasestorage.app",
-  messagingSenderId: "372466566602",
-  appId: "1:372466566602:web:ea5e7e90db5ea7e9aafb33"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
