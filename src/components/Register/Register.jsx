7cbf197b4ac0243b5c9abcd2bafc849a42167505
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-05-21 20:56:49 +0600

    complete provider and register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable no-unused-vars */
 import React, { use } from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import { AuthContext } from '../../contexts/AuthContext';
 import Swal from 'sweetalert2';
 
 const Register = () => {
-
-  const { createUser } = use(AuthContext);
+  const { createUser, updateNamePhoto, signInWithGoogle, setUser } = use(AuthContext);
+  const navigate = useNavigate();
 
   const handleSignUp = e => {
     e.preventDefault();
@@ -16,7 +17,9 @@ const Register = () => {
 
     createUser(email, password)
       .then(result => {
-        console.log(result);
+        const user = result.user;
+        setUser(user);
+        form.reset();
         Swal.fire({
           title: 'Successfully Registered!',
           icon: 'success',
@@ -28,6 +31,40 @@ const Register = () => {
             popup: 'my-swal-dark'
           }
         })
+        navigate('/');
+      }).catch(error => {
+        console.log(error);
+        Swal.fire({
+          title: error.message,
+          icon: 'error',
+          background: '#1e1e1e',
+          color: 'white', 
+          confirmButtonColor: 'red', 
+          customClass: {
+            popup: 'my-swal-dark'
+          }
+        });
+        navigate('/');
+      })
+  }
+
+  const handleGoogleSignUp = () => {
+    signInWithGoogle()
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        Swal.fire({
+          title: 'Successfully Logged In!',
+          icon: 'success',
+          background: '#1e1e1e',
+          color: '#a0ffb0', 
+          iconColor: '#00ff88', 
+          confirmButtonColor: '#00c472', 
+          customClass: {
+            popup: 'my-swal-dark'
+          }
+        })
+        navigate('/');
       }).catch(error => {
         console.log(error);
         Swal.fire({
@@ -40,6 +77,7 @@ const Register = () => {
             popup: 'my-swal-dark'
           }
         })
+        navigate('/');
       })
   }
 
@@ -62,7 +100,7 @@ const Register = () => {
           <input type="password" className="input" placeholder="Password" name='password' />
 
           <button className="btn btn-neutral mt-4 mb-2" type='submit'>Register</button>
-          <button className="btn bg-white text-black border-[#e5e5e5]" type='button'>
+          <button className="btn bg-white text-black border-[#e5e5e5]" type='button' onClick={handleGoogleSignUp}>
             <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
             Login with Google
           </button>
--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, updateProfile } from 'firebase/auth';
 import { auth } from '../firebase/firebase.init';
 
 const AuthProvider = ({children}) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const provider = new GoogleAuthProvider();
 
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   }
 
-  
+  const updateNamePhoto = (displayName, photoURL) => {
+    return updateProfile(auth.currentUser, {
+      displayName: displayName,
+      photoURL: photoURL
+    })
+  }
+
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  }
+
+  useEffect(() => {
+    const unsubscribed = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    })
+    return () => {
+      unsubscribed();
+    }
+  }, [])
 
   const userInfo = {
+    user,
+    setUser,
     createUser,
+    updateNamePhoto,
+    signInWithGoogle,
+    loading,
+    setLoading
   }
 
   return (
