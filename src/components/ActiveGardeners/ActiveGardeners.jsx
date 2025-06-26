daf29b476bb3aee314af3dd56fc4724f776a0eaf
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-06-26 15:01:52 +0600

    test

--- src/components/ActiveGardeners/ActiveGardeners.jsx
+++ src/components/ActiveGardeners/ActiveGardeners.jsx
@@ -5,7 +5,7 @@ import './ActiveGardeners.css';
 const ActiveGardeners = ({gardenersPromise}) => {
   const data = use(gardenersPromise);
   return (
-    <div className='w-[95vw] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12' id='active-gardeners'>
+    <div className='w-[95vw] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12' id='active-gardeners'>
       {data.map((gardener, i) => (
         <ActiveGardeningCard key={i} gardener={gardener}></ActiveGardeningCard>
       ))}
