b5287878138787bb68124c2d34da1aa9a77843cd
SafuGit <safwan55.ah@gmail.com>
2025-06-27 14:36:47 +0600

    add aboutUs and contactUs

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,6 +10,12 @@ const NavBar = () => {
   const location = useLocation();
   const isHomePage = location.pathname === '/' || location.pathname === '/gardeners' || location.pathname === '/dashboard';
 
+  const navItems = <>
+    <li><NavLink to={'/'}>Home</NavLink></li>
+    <li><NavLink to={'/exploreGardeners'}>All Gardeners</NavLink></li>
+    <li><NavLink to={'/aboutUs'}>About Us</NavLink></li>
+  </>
+
   const handleSignOut = () => {
     logOut()
       .then(() => {
@@ -41,7 +47,7 @@ const NavBar = () => {
 
   return (
     <div className={`navbar bg-base-200 shadow-sm border-base-300 border-b ${isHomePage ? "" : "mb-20"}`}>
-      <div className="navbar-start">
+      <div className="navbar-start ml-[1vw]">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
@@ -73,7 +79,7 @@ const NavBar = () => {
             <li><NavLink to={'/myTips'}>My Tips</NavLink></li> </div> : ""}
         </ul>
       </div>
-      <div className="navbar-end">
+      <div className="navbar-end sm:mr-[1vw]">
         <div className='hidden sm:block'>
           <ThemeChange></ThemeChange>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,9 @@ import UpdateTips from './components/UpdateTips/UpdateTips'
 import ExploreGardeners from './components/ExploreGardeners/ExploreGardeners'
 import Dashboard from './components/Dashboard/Dashboard'
 import Overview from './components/Dashboard/Overview/Overview'
+import { BiPhone } from 'react-icons/bi'
+import { CgMail } from 'react-icons/cg'
+import Swal from 'sweetalert2'
 
 const router = createBrowserRouter([
   {path: '/', Component: Root, children: [
@@ -88,6 +91,45 @@ const router = createBrowserRouter([
       Component: ExploreGardeners,
       loader: () => fetch('https://gardenhub-server-nine.vercel.app/gardeners'),
       hydrateFallbackElement: <Loading></Loading>
+    },
+    {
+      path: '/aboutUs',
+      Component: () => <div className="max-w-5xl mx-auto px-6 py-12">
+        <h1 className="text-4xl font-bold text-center mb-8">About Us</h1>
+        <div className="bg-base-200 border border-green-200 p-6 md:p-10 rounded-2xl shadow-md">
+          <p className="text-lg leading-relaxed">
+            <strong>GardenHub</strong> is a vibrant community platform where gardeners of all levels connect, share tips,
+            and inspire growth. From urban balconies to sprawling backyards, our mission is to cultivate knowledge,
+            creativity, and a passion for sustainable gardening. Whether you’re a seasoned expert or just planting your
+            first seed, GardenHub is your trusted companion in every step of your green journey.
+          </p>
+        </div>
+      </div>
+    },
+    {
+      path: '/contactUs',
+      Component: () => <div className='w-[95vw] mx-auto'>
+        <h1 className='text-4xl mb-8'>About Us</h1>
+        <div className='flex gap-4'>
+          <div className='card bg-base-200 shadow-xl p-8 w-fit'>
+            <h1 className='card-title text-2xl mb-4'>Give us a call</h1>
+            <p className='flex items-center gap-2'><BiPhone></BiPhone> +8801981575920 <br /> +8801710888010</p>
+          </div>
+          <div className='card bg-base-200 shadow-xl p-8 w-fit'>
+            <h1 className='card-title text-2xl truncate mb-4'>Or Contact through Email</h1>
+            <p className='flex items-center gap-2'><CgMail></CgMail> @safwan55.ah@gmail.com <br /> @safwansadid.ah@gmail.com</p>
+          </div>
+          <div className='card bg-base-200 shadow-xl p-8 w-full'>
+            <h1 className='card-title text-2xl mb-2'>Give us some feedback!</h1>
+            <textarea className='input rounded-xl w-full mb-4'></textarea>
+            <button className='btn btn-primary w-fit' onClick={() => {
+              Swal.fire({
+                title: 'Thank you for your feedback!',
+              })
+            }}>Submit</button>
+          </div>
+        </div>
+      </div>
     }
   ]},
   {
